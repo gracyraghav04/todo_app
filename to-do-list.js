6a5839b362944b2307f7c10aee1497adc53c8258
gracy raghav <gracyraghav.04@gmail.com>
2026-06-24 15:11:36 +0530

    added local storage feature

--- to-do-list.js
+++ to-do-list.js
@@ -1,4 +1,4 @@
-console.log("JS connected");
+/*console.log("JS connected");
 
 const  btn = document.getElementById("btn");
 
@@ -37,4 +37,77 @@ btn.addEventListener("click", () => {
     document.getElementById("list").appendChild(li);
 
     document.getElementById("task").value = "";
+});*/
+console.log("JS connected");
+
+const btn = document.getElementById("btn");
+const input = document.getElementById("task");
+const list = document.getElementById("list");
+
+
+let savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+
+function renderTasks() {
+    list.innerHTML = "";
+
+    savedTasks.forEach((taskText, index) => {
+        const li = document.createElement("li");
+        li.classList.add("item");
+
+        const text = document.createElement("span");
+        text.innerText = taskText;
+
+        const delbtn = document.createElement("button");
+        delbtn.innerText = "Remove";
+
+        const checkbtn = document.createElement("input");
+        checkbtn.type = "checkbox";
+
+        
+        delbtn.addEventListener("click", () => {
+            savedTasks.splice(index, 1);
+            updateStorage();
+            renderTasks();
+        });
+
+  
+        checkbtn.addEventListener("change", () => {
+            if (checkbtn.checked) {
+                setTimeout(() => {
+                    savedTasks.splice(index, 1);
+                    updateStorage();
+                    renderTasks();
+                }, 800);
+            }
+        });
+
+        li.appendChild(text);
+        li.appendChild(delbtn);
+        li.appendChild(checkbtn);
+
+        list.appendChild(li);
+    });
+}
+
+
+function updateStorage() {
+    localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+
+btn.addEventListener("click", () => {
+    const value = input.value.trim();
+
+    if (value === "") return;
+
+    savedTasks.push(value);
+
+    input.value = "";
+
+    updateStorage();
+    renderTasks();
 });
+
+
+renderTasks();
